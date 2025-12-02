7111fddde628a10be43601af7efadcd68f2db82b
nicolasarcidiacono <angelonilaureano@gmail.com>
2025-12-02 19:44:54 -0300

    Loginview
    Formulario de ingreso al sistema y de verificacion de ticket
    
    Co-Authored-By: Emiii <emilianocarrasco60@gmail.com>

---
+++ resources/views/login/ingreso.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <div class="card mt-3">
+                    <div class="card-body">
+                        <h1>Ingreso:</h1>
+                        <form action="" method="post">
+                            <div class="mb-3">
+                                <label for="usuario" class="form-label">Usuario:</label>
+                                <input type="text" class="form-control" id="usuario" name="usuario">
+                            </div>
+                            <div class="mb-3">
+                                <label for="contraseña" class="form-label">Contraseña:</label>
+                                <input type="password" class="form-control" id="contraseña" name="contraseña">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Ingresar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <div class="card mt-3">
+                    <div class="card-body ">
+                        <h1>Verificacion:</h1>
+                        <form action="" method="post">
+                            <div class="mb-3">
+                                    <label  class="form-label" for="codigo">Ingrese su codigo:</label>
+                                    <input type="text" class="form-control" id="codigo" name="codigo">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Buscar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
