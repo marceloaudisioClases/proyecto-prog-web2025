e4f7d546ae530ef62102ae77251db129c05396a6
Hilet Aulas <hilet@HILET.LOCAL>
2025-12-04 19:07:39 -0300

    usuarios visual
    añadi un controller de la vista de informacion de usuarios

--- resources/views/Usuarios/newuser.blade.php
+++ resources/views/Usuarios/newuser.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Registro de Usuario</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
@@ -15,21 +15,12 @@
             <div class="card-body">
                     <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">@</span>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                    <input type="text" class="form-control" placeholder="Usuario" aria-label="Usuario" aria-describedby="basic-addon1">
                   </div>
 
                   <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Recipient’s username" aria-label="Recipient’s username" aria-describedby="basic-addon2">
+                    <input type="text" class="form-control" placeholder="Correo de Usuario" aria-label="Correo de Usuario" aria-describedby="basic-addon2">
                     <span class="input-group-text" id="basic-addon2">@example.com</span>
-                  </div>
-
-                  <div class="mb-3">
-                    <label for="basic-url" class="form-label">Your vanity URL</label>
-                    <div class="input-group">
-                      <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-                      <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4">
-                    </div>
-                    <div class="form-text" id="basic-addon4">Example help text goes outside the input group.</div>
                   </div>
 
                   <div class="input-group mb-3">
