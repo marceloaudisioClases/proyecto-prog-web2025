4c30c62c847de3b7e5f140a0224cb4a0f920e4d3
Hilet Aulas <hilet@HILET.LOCAL>
2025-12-04 20:01:48 -0300

    vista de usuario
    solo modifique partes visuales

--- resources/views/Usuarios/newuser.blade.php
+++ resources/views/Usuarios/newuser.blade.php
@@ -1,6 +1,13 @@
-@extends
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registro de Usuario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
   <body>
-                                       <h1 class="text-center">Usuario</h1>
+                                       <h1 class="text-center">Informacion de Usuario</h1>
     <div class="container">
       <div class="row">
         <div class="col">
@@ -12,24 +19,22 @@
                   </div>
 
                   <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon2">@ejemplo.com</span>
                     <input type="text" class="form-control" placeholder="Correo de Usuario" aria-label="Correo de Usuario" aria-describedby="basic-addon2">
-                    <span class="input-group-text" id="basic-addon2">@example.com</span>
                   </div>
 
                   <div class="input-group mb-3">
-                    <span class="input-group-text">$</span>
+                    <span class="input-group-text">Estado</span>
                     <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-                    <span class="input-group-text">.00</span>
                   </div>
 
                   <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username">
-                    <span class="input-group-text">@</span>
+                    <span class="input-group-text">Fecha de Ingreso</span>
                     <input type="text" class="form-control" placeholder="Server" aria-label="Server">
                   </div>
 
                   <div class="input-group">
-                    <span class="input-group-text">With textarea</span>
+                    <span class="input-group-text">Ticket/Reclamo</span>
                     <textarea class="form-control" aria-label="With textarea"></textarea>
                   </div>
             </div>
@@ -37,4 +42,6 @@
         </div>
       </div>
     </div>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
