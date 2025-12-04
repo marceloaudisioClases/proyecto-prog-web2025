fefe40919ec4aed9483d93960a0f095792fbeb55
Hilet Aulas <hilet@HILET.LOCAL>
2025-12-04 19:34:25 -0300

    vista de usuario
    vista de la informacion de usuario y su controlador

--- resources/views/Usuarios/newuser.blade.php
+++ resources/views/Usuarios/newuser.blade.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registro de Usuario</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
+@extends
   <body>
                                        <h1 class="text-center">Usuario</h1>
     <div class="container">
@@ -44,6 +37,4 @@
         </div>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
-</html>
+    
