a0f4656b217e5ee3d87a50a91d758874cbb35c53
laureano <angelonilaureano@gmail.com>
2025-12-11 19:39:14 -0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
            $request->session()->regenerate();
            return redirect()->intended(route("principal"));
         }else{
-            return redirect()->back()->with("error","Credenciales Incorrectas");
+            return redirect()->back()->withErrors("Credenciales Incorrectas");
         }
     }
 }
