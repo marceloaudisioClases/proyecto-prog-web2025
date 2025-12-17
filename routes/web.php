389f5afcd36c488aed2682c74b042a5446d7458e
Martina_Casco <martinacasco40@gmail.com>
2025-12-17 16:27:37 -0300

    gestion de usuarios

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\UserController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*
 
 Route::get('/',[AuthController::class,'login'])->name('login');
 Route::post('/',[AuthController::class,'ingreso'])->name('login.ingreso');
